a9ca87a63f254fe23a28843cb0f6f34d1574db51
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-03 16:45:25 +0530

    add product function

--- functions.java
+++ functions.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class functions {
-   public static int calculateSum(int a, int b){
-    int sum = a + b;
-    return sum;
+   public static int calculateProduct(int a, int b){
+    
+    return a*b;
    }
     
     public static void main(String[] args) {
@@ -10,8 +10,7 @@ public class functions {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int sum = calculateSum(a, b);
-        System.out.println(sum);
+        System.out.println(calculateProduct(a, b));
     }
     
 }
