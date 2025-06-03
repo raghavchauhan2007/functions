dea2071b9d827793ee0e33b11a48d31c5a0d3553
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-03 16:39:47 +0530

    add function calculateSum

--- functions.java
+++ functions.java
@@ -1,14 +1,17 @@
 import java.util.*;
 public class functions {
-   
-    public static void printMyName(String name){
-    System.out.println(name);
-    return;
-    }
+   public static int calculateSum(int a, int b){
+    int sum = a + b;
+    return sum;
+   }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        printMyName(name);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = calculateSum(a, b);
+        System.out.println(sum);
     }
     
 }
