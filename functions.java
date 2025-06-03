9dbf0a91c90999e592ad136bf897730cd500d1a7
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-03 17:16:50 +0530

    add factorial function

--- functions.java
+++ functions.java
@@ -4,13 +4,21 @@ public class functions {
     
     return a*b;
    }
-    
+
+   public static void printFactorial(int n){
+    int factorial = 1;
+    for(int i = n; i>=1; i--){
+        factorial = factorial*i;
+    }
+    System.out.println(factorial);
+    return;
+   }
+           
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println(calculateProduct(a, b));
+        int n = sc.nextInt();
+        printFactorial(n);
+        
     }
     
 }
