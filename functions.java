4f975b6e7451036188159c003ea0ac24a7c5056a
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-04 17:13:14 +0530

    last

--- functions.java
+++ functions.java
@@ -1,19 +1,21 @@
-import java.util.*;
+// import java.util.*;
 
-public class functions {
+// public class functions {
+//     public static int greatestNumber(int a , int b){
+//         if(a>b){
+//             return a;
+//         }
+//         else{
+//             return b;
+//         }
+        
+//     }
+// }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
 
-    public static int oddSum(int n){
-    int sum = 0;
-    for(int i = 1; i<=n; i=i+2){
-        sum = sum +i;
-    }
-    return sum;
-}
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+//         System.out.println();
+//     }
 
-        int sum = oddSum(n);
-        System.out.println(sum);
-    }
-}
