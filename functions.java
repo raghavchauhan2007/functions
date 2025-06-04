e4e1a40187ad154edcfe2d5ac4dce12b985093e1
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-04 16:48:43 +0530

    add oddSum function

--- functions.java
+++ functions.java
@@ -2,15 +2,18 @@ import java.util.*;
 
 public class functions {
 
-    public static int average(int a, int b, int c){
-        return(a+b+c)/3;
+    public static int oddSum(int n){
+    int sum = 0;
+    for(int i = 1; i<=n; i=i+2){
+        sum = sum +i;
     }
+    return sum;
+}
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int n = sc.nextInt();
 
-        System.out.println(average(a, b, c));
+        int sum = oddSum(n);
+        System.out.println(sum);
     }
 }
