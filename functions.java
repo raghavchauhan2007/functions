7a7b3a1139571f1948195a27f7bec4410010142d
Raghav Chauhan <chauhan.raghav2020@gmail.com>
2025-06-04 18:37:31 +0530

    vote eligibility

--- functions.java
+++ functions.java
@@ -1,14 +1,19 @@
 import java.util.*;
 
 public class functions {
-    public static double circumference(int r){
-        return Math.PI*2*r;
+    public static String checkAge(int age){
+        if(age>=18){
+            return "eligible to vote";
+        }
+        else{
+            return "not eligible to vote";
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt();
-        System.out.println(circumference(r));
+        int age = sc.nextInt();
+        System.out.println(checkAge(age));
     }
 
 }
